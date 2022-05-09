e98f3ca6e804789b2697e22a015283a33af78b14
Sidali <gh.fullstack@gmail.com>
2022-05-09 21:31:09 +0100

    protect post

--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const postControllers = require('../controllers/post')
-
+const protect = require('../middleware/authMiddleware')
 const router = express.Router()
 
-router.post('/add-post', postControllers.addPost)
-router.get('/posts', postControllers.getPosts)
-router.get('/:postId', postControllers.getPostById)
-router.put('/update/:postId', postControllers.updatePost)
-router.delete('/delete/:postId', postControllers.deletePost)
+router.post('/add-post', protect,postControllers.addPost)
+router.get('/posts', protect,postControllers.getPosts)
+router.get('/:postId', protect,postControllers.getPostById)
+router.put('/update/:postId', protect,postControllers.updatePost)
+router.delete('/delete/:postId', protect,postControllers.deletePost)
 
 module.exports = router
